96d4a35d6a1d5a63c7aecb00b964693e036ec02d
arofaanonna <arofaanonna@gmail.com>
2026-02-15 22:13:27 +0600

    temperature converter

--- practise1.cpp
+++ practise1.cpp
@@ -1,43 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    double num1, num2;
-    char op;
-
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter operator (+, -, *, /, %): ";
-    cin >> op;
-    cout << "Enter second number: ";
-    cin >> num2;
-
-    switch (op)
-    {
-    case '+':
-        cout << "Result: " << num1 + num2 << endl;
-        break;
-    case '-':
-        cout << "Result: " << num1 - num2 << endl;
-        break;
-    case '*':
-        cout << "Result: " << num1 * num2 << endl;
-        break;
-    case '/':
-        if (num2 != 0)
-            cout << "Result: " << num1 / num2 << endl;
-        else
-            cout << "Error: Division by zero!" << endl;
-        break;
-    case '%':
-        if ((int)num2 != 0)
-            cout << "Result: " << (int)num1 % (int)num2 << endl;
-        else
-            cout << "Error: Division by zero!" << endl;
-        break;
-    default:
-        cout << "Invalid operator!" << endl;
+int main() {
+    double temp, result;
+    int choice;
+    
+    cout << "1. Celsius to Fahrenheit" << endl;
+    cout << "2. Fahrenheit to Celsius" << endl;
+    cout << "Enter choice (1 or 2): ";
+    cin >> choice;
+    
+    if(choice == 1) {
+        cout << "Enter temperature in Celsius: ";
+        cin >> temp;
+        result = (temp * 9.0/5.0) + 32;
+        cout << temp << "C = " << result << "F" << endl;
+    }
+    else if(choice == 2) {
+        cout << "Enter temperature in Fahrenheit: ";
+        cin >> temp;
+        result = (temp - 32) * 5.0/9.0;
+        cout << temp << "F = " << result << "C" << endl;
+    }
+    else {
+        cout << "Invalid choice!" << endl;
     }
     return 0;
 }
