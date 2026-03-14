b838f66d5a5711e49c2865993985b26403c14634
arofaanonna <arofaanonna@gmail.com>
2026-03-14 12:53:38 +0600

    Refactor code structure for improved readability and maintainability

---
+++ 3/stackInvector.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector <int>block;
+class stack{
+    public:
+    void push(int val) {
+        block.push_back(val);
+    }
+
+    void pop(){
+        block.pop_back();
+    }
+
+    int top(){
+        return block[block.size()-1];
+    }
+    bool empty(){
+        return block.empty();
+    }
+};
+int main()
+{
+   stack s;
+   s.push(10);
+   s.push(20);
+   s.push(30);
+   s.push(40);
+   s.push(50);
+   s.push(60);
+
+   while(!s.empty()){
+    cout<<s.top()<<" ";
+    s.pop();
+   }
+cout<<endl;
+return 0;
+
+}
